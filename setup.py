fb7fdbcb09e16748ce930f62074de313bbc14379
sambit-giri <sambit.giri@gmail.com>
2024-01-19 15:19:15 +0100

    updated ******************************************************************************** Usage of dash-separated 'description-file' will not be supported in future versions. Please use the underscore name 'description_file' instead.
    By 2024-Sep-26, you need to update your project and remove deprecated calls
    or your builds will no longer be supported.

--- setup.py
+++ setup.py
@@ -8,19 +8,25 @@ from setuptools import setup, find_packages
 #from distutils.core import setup
 
 
-setup(name='tools_cosmo',
-      version='0.0.2',
-      author='Sambit Giri',
-      author_email='sambit.giri@su.se',
-      packages=find_packages("src"),
-      package_dir={"": "src"},
-      package_data={'tools_cosmo': ['input_data/*']},
-      install_requires=['numpy', 'scipy', 'matplotlib', 'pytest', 
-                        'astropy', 'scikit-learn', 
-                        'git+https://github.com/sambit-giri/tools_cosmo.git', #'baccoemu', 
-                        'camb'
-                        ],
-      include_package_data=True,
-      long_description=open('README.md').read(),
-      long_description_content_type='text/markdown',
+setup(
+    name='tools_cosmo',
+    version='0.0.2',
+    author='Sambit Giri',
+    author_email='sambit.giri@su.se',
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    package_data={'tools_cosmo': ['input_data/*']},
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'pytest',
+        'astropy',
+        'scikit-learn',
+        'git+https://github.com/sambit-giri/tools_cosmo.git',
+        'camb'
+    ],
+    include_package_data=True,
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
 )
