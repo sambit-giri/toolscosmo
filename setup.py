2e03052fae7310185f7af75926b01bbd256f205b
sambit-giri <sambit.giri@gmail.com>
2024-01-19 15:26:48 +0100

    updated

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ Setup script
 from setuptools import setup, find_packages
 #from distutils.core import setup
 
+# Read requirements from requirements.txt
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 
 setup(
     name='tools_cosmo',
@@ -16,16 +20,17 @@ setup(
     packages=find_packages("src"),
     package_dir={"": "src"},
     package_data={'tools_cosmo': ['input_data/*']},
-    install_requires=[
-        'numpy',
-        'scipy',
-        'matplotlib',
-        'pytest',
-        'astropy',
-        'scikit-learn',
-        'baccoemu',
-        'camb'
-    ],
+    install_requires=requirements,
+#     install_requires=[
+#         numpy',
+#         'scipy',
+#         'matplotlib',
+#         'pytest',
+#         'astropy',
+#         'scikit-learn',
+#         'baccoemu',
+#         'camb'
+#     ],
     include_package_data=True,
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
